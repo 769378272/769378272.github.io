d1c09bf7d63efd9310894315905dd142456c3cb5
san <769378272@qq.com>
2025-05-31 15:27:22 +0000

    Site updated: 2025-05-31 15:27:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["san/hello-world/","san/PVE-安装-Ubuntu/","san/windows破解/"];function toRandomPost(){
+var posts=["san/CF薅羊毛隧道套SaaS/","san/PVE-安装-Ubuntu/","san/hello-world/","san/windows破解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
