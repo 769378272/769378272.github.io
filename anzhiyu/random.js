a29d91efef21e01140ce23611c932c58e952c1ae
san <769378272@qq.com>
2025-06-16 02:27:38 +0000

    Site updated: 2025-06-16 02:27:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["san/CF薅羊毛隧道套SaaS/","san/PVE-安装-Ubuntu/","san/windows破解/"];function toRandomPost(){
+var posts=["san/CF薅羊毛隧道套SaaS/","san/NATMap部署Web服务/","san/windows破解/","san/PVE-安装-Ubuntu/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
