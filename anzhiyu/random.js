1f911fa9bd03273689ac050a8fbe595d594b7da4
san <769378272@qq.com>
2025-07-04 07:01:46 +0000

    Site updated: 2025-07-04 07:01:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["san/CF薅羊毛隧道套SaaS/","san/Windows-node-环境安装/","san/windows破解/","san/PVE-安装-Ubuntu/","san/Windows-JVMS-JDK管理工具/","san/NATMap部署Web服务/"];function toRandomPost(){
+var posts=["san/CF薅羊毛隧道套SaaS/","san/Internet Download Manager破解/","san/NATMap部署Web服务/","san/PVE-安装-Ubuntu/","san/Windows-JVMS-JDK管理工具/","san/Windows-node-环境安装/","san/windows破解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
